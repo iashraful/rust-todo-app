72c838ed26e96ad3e32fdda4e9e8148aee0cb1cf
Mohammad Ashraful <a5hraful@outlook.com>
2024-05-07 23:46:29 +0600

    Wanted to making json serializable but failed for traits

---
+++ src/todo/services.rs
@@ -0,0 +1,10 @@
+use crate::todo::types::Todo;
+
+pub struct TodoService {}
+
+impl TodoService {
+    fn create(todo: Todo) {
+        println!("Creating the task to the db.")
+
+    }
+}
--- src/todo/types.rs
+++ src/todo/types.rs
@@ -1,13 +1,29 @@
 use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct BaseModel {
+    pub created_at: Option<DateTime<Local>>,
+    pub updated_at: Option<DateTime<Local>>,
+}
+
+impl BaseModel {
+    fn json(&mut self) {
+        if self.created_at == None {
+            self.created_at = Some(Local::now())
+        }
+        if self.updated_at == None {
+            self.updated_at = Some(Local::now())
+        }
+        serde_json::to_string(&self).unwrap();
+    }
+}
 
 #[derive(Debug)]
 pub struct Label {
     pub id: u32,
     pub name: String,
-    pub created_at: Option<DateTime<Local>>,
-    pub updated_at: Option<DateTime<Local>>,
 }
-
 
 #[derive(Debug)]
 pub struct Todo {
@@ -16,8 +32,6 @@ pub struct Todo {
     pub description: String,
     pub label: Option<Label>,
     pub is_checked: bool,
-    pub created_at: Option<DateTime<Local>>,
-    pub updated_at: Option<DateTime<Local>>,
 }
 
 impl Todo {
