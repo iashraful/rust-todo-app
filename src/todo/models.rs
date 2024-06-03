a22361422c4947e4c00291ee69a43b669cbf6c48
Mohammad Ashraful <ashrafulrobin3@gmail.com>
2024-06-03 09:49:52 +0600

    Added migrations manually so generated the schema

--- src/todo/models.rs
+++ src/todo/models.rs
@@ -1,21 +1,25 @@
 use diesel::prelude::*;
-use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable, Insertable, Serialize, Deserialize)]
-#[diesel(table_name = crate::schema::labels)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Queryable)]
+#[diesel(table_name = crate::todo::schema::labels)]
 pub struct Label {
     pub id: u32,
     pub name: String,
 }
 
-#[derive(Queryable, Selectable, Insertable, Serialize, Deserialize)]
-#[diesel(table_name = crate::3schema::todos)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Insertable)]
+#[diesel(table_name = crate::todo::schema::labels)]
+pub struct NewLabel {
+    pub name: String,
+}
+
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::todo::schema::todos)]
 pub struct Todo {
     pub id: u32,
     pub title: String,
     pub description: String,
-    pub label: Option<Label>,
+    pub label_id: Option<u32>,
     pub is_checked: bool,
 }
--- src/todo/schema.rs
+++ src/todo/schema.rs
@@ -1,2 +1,29 @@
 // @generated automatically by Diesel CLI.
 
+diesel::table! {
+    use diesel::sql_types::*;
+
+    labels (id) {
+        id -> Int4,
+        name -> Varchar,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+
+    todos (id) {
+        id -> Int4,
+        title -> Varchar,
+        description -> Nullable<Text>,
+        label_id -> Int4,
+        is_checked -> Bool,
+    }
+}
+
+diesel::joinable!(todos -> labels (label_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    labels,
+    todos,
+);
