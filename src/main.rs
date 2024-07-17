f0c23aafc17b5b4be242992cacd23a032b4001d5
Mohammad Ashraful <a5hraful@outlook.com>
2024-07-17 14:12:44 +0600

    Updated server running code to seperate function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use axum;
 use deadpool_diesel::{Manager, Pool};
 use diesel::PgConnection;
 use dotenvy::dotenv;
-use tokio::net::TcpListener;
 
-use api::routes::create_api_router;
+use api::server::run_server;
 use core::config::config;
 use core::db;
 use core::logging::LogManager;
@@ -25,13 +23,6 @@ async fn main() {
     // Connecting to Postgres DB
     let conn_pool: Pool<Manager<PgConnection>> =
         db::establish_connection(config.db_url().to_string());
-
-    let host: &str = config.server_host();
-    let port: u16 = config.server_port();
-    let address: String = format!("{}:{}", host, port);
-
-    let router: axum::Router = create_api_router(conn_pool);
-    println!("Listening on http://{}", address);
-    let listener: TcpListener = TcpListener::bind(address).await.unwrap();
-    axum::serve(listener, router).await.unwrap();
+    
+    run_server(config, conn_pool).await;
 }
